a185efd2d7089d408c553188e78359900d258a68
anonystick <anonystick@gmail.com>
2023-02-04 09:40:39 +0700

    CREATE: init project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Yuotube 2023
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/c/TipsJavascript/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
